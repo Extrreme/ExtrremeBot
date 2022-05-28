00db77a7fa7d5383ff116e017d3287ce834c8eb0
Extrreme <jeremy.peters8@gmail.com>
2022-05-27 22:08:58 -0400

    Registered clear queue command

--- src/main/java/dev/extrreme/extrremebot/ExtrremeBot.java
+++ src/main/java/dev/extrreme/extrremebot/ExtrremeBot.java
@@ -4,6 +4,7 @@ import dev.extrreme.extrremebot.base.DiscordBot;
 import dev.extrreme.extrremebot.audio.MusicManagerManager;
 import dev.extrreme.extrremebot.commands.misc.HelpCommand;
 import dev.extrreme.extrremebot.commands.misc.StockCommand;
+import dev.extrreme.extrremebot.commands.music.MusicClearCommand;
 import dev.extrreme.extrremebot.commands.music.MusicPlayCommand;
 import dev.extrreme.extrremebot.commands.music.MusicRepeatCommand;
 import dev.extrreme.extrremebot.commands.music.MusicSkipCommand;
@@ -27,6 +28,7 @@ public class ExtrremeBot extends DiscordBot {
         registerCommand(new MusicPlayCommand());
         registerCommand(new MusicSkipCommand());
         registerCommand(new MusicRepeatCommand());
+        registerCommand(new MusicClearCommand());
 
         registerCommand(new TrackerCommand());
     }
